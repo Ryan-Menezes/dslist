bfb378d01a57218d3df8164815c11734f404e456
Ryan de Menezes <menezesryan1010@gmail.com>
2025-01-23 16:18:16 -0300

    Get lists

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.services.GameService;
 
@@ -16,6 +18,13 @@ import com.devsuperior.dslist.services.GameService;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value ="/{id}")
+    public ResponseEntity<GameDTO> findAById(@PathVariable Long id) {
+        var game = gameService.findById(id);
+
+        return ResponseEntity.ok(game);
+    }
 
     @GetMapping
     public ResponseEntity<List<GameMinDTO>> findAll() {
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -13,6 +15,15 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        var game = gameRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Not found"));
+
+        return new GameDTO(game);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var games = gameRepository.findAll();
         return games.stream().map(x -> new GameMinDTO(x)).toList();
